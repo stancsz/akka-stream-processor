a455cd10f03e065020be2ad1edd8023b64a710c6
Stan Chen <stanleychen587@gmail.com>
2021-02-09 23:55:09 -0700

    imported code from git@github.com:stancsz/alpakka-samples-alpakka-sample-kafka-to-elasticsearch.git

--- build.sbt
+++ build.sbt
@@ -9,6 +9,11 @@ lazy val AlpakkaVersion = "2.0.1"
 lazy val AkkaVersion    = "2.6.12"
 lazy val AlpakkaKafkaVersion = "2.0.5"
 
+
+val scalaVer = "2.13.3"
+// #deps
+
+
 lazy val root = (project in file(".")).
   settings(
     inThisBuild(List(
@@ -29,6 +34,24 @@ lazy val root = (project in file(".")).
 
       // Logging
       "com.typesafe.akka" %% "akka-slf4j"               % AkkaVersion,
-      "ch.qos.logback"    % "logback-classic"           % "1.2.3"
+      "ch.qos.logback"    % "logback-classic"           % "1.2.3",
+
+      // #deps
+      "com.lightbend.akka" %% "akka-stream-alpakka-elasticsearch" % AlpakkaVersion,
+      "com.typesafe.akka" %% "akka-stream-kafka" % AlpakkaKafkaVersion,
+      "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-actor" % AkkaVersion,
+      // for JSON in Scala
+      "io.spray" %% "spray-json" % "1.3.5",
+      // for JSON in Java
+      "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8" % "2.10.5",
+      "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.10.5",
+      // Logging
+      "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
+      "ch.qos.logback" % "logback-classic" % "1.2.3",
+      // #deps
+      "org.testcontainers" % "elasticsearch" % "1.14.3",
+      "org.testcontainers" % "kafka" % "1.14.3"
     )
   )
---
+++ src/main/scala/kafka/consumer/Main.scala
@@ -0,0 +1,85 @@
+package kafka.consumer
+
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.kafka.scaladsl.{Committer, Consumer}
+import akka.kafka.{CommitterSettings, ConsumerSettings, Subscriptions}
+import akka.stream.alpakka.elasticsearch.WriteMessage
+import akka.stream.alpakka.elasticsearch.scaladsl.ElasticsearchFlow
+import akka.stream.scaladsl.Sink
+import akka.{Done, NotUsed}
+import org.apache.http.HttpHost
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.{IntegerDeserializer, StringDeserializer}
+import org.elasticsearch.client.RestClient
+
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+object Main extends App with Helper {
+
+  implicit val actorSystem: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "KafkaToElasticSearch")
+  implicit val executionContext: ExecutionContext = actorSystem.executionContext
+
+  val topic = "movies-to-elasticsearch"
+  private val groupId = "docs-group"
+
+  // #es-setup
+  // Elasticsearch client setup (4)
+  implicit val elasticsearchClient: RestClient =
+  RestClient
+    .builder(HttpHost.create(elasticsearchAddress))
+    .build()
+
+  val indexName = "movies"
+  // #es-setup
+
+  // #kafka-setup
+  // configure Kafka consumer (1)
+  val kafkaConsumerSettings = ConsumerSettings(actorSystem.toClassic, new IntegerDeserializer, new StringDeserializer)
+    .withBootstrapServers(kafkaBootstrapServers)
+    .withGroupId(groupId)
+    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+    .withStopTimeout(0.seconds)
+  // #kafka-setup
+
+  private def readFromKafkaWriteToElasticsearch() = {
+    // #flow
+    val control: Consumer.DrainingControl[Done] = Consumer
+      .sourceWithOffsetContext(kafkaConsumerSettings, Subscriptions.topics(topic)) // (5)
+      .map { consumerRecord => // (6)
+        val movie = consumerRecord.value().parseJson.convertTo[Movie]
+        WriteMessage.createUpsertMessage(movie.id.toString, movie)
+      }
+      .via(ElasticsearchFlow.createWithContext(indexName, "_doc")) // (7)
+      .map { writeResult => // (8)
+        writeResult.error.foreach { errorJson =>
+          throw new RuntimeException(s"Elasticsearch update failed ${writeResult.errorReason.getOrElse(errorJson)}")
+        }
+        NotUsed
+      }
+      .via(Committer.flowWithOffsetContext(CommitterSettings(actorSystem.toClassic))) // (9)
+      .toMat(Sink.ignore)(Consumer.DrainingControl.apply) // (10)
+      .run()
+    // #flow
+    control
+  }
+
+  val movies = List(Movie(23, "Psycho"), Movie(423, "Citizen Kane"))
+  val writing: Future[Done] = writeToKafka(topic, movies)
+  Await.result(writing, 10.seconds)
+
+  val control = readFromKafkaWriteToElasticsearch()
+  // Let the read/write stream run a bit
+  Thread.sleep(5.seconds.toMillis)
+  val copyingFinished = control.drainAndShutdown()
+  Await.result(copyingFinished, 10.seconds)
+
+  for {
+    read <- readFromElasticsearch(indexName)
+  } {
+    read.foreach(m => println(s"read $m"))
+    stopContainers()
+    actorSystem.terminate()
+  }
+}
