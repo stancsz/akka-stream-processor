80da87299301645bfc005d9982f8cf0da7c50eea
Stan Chen <stanleychen587@gmail.com>
2021-02-17 18:37:40 -0700

    configured setup scripts and managed to append received messages in a list

--- setup/setup_script.sh
+++ setup/setup_script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Comment out by default
+#docker kill $(docker ps -q) # stop all containers:
+#docker rm $(docker ps -a -q) # remove all containers
+#docker rmi $(docker images -q) # remove all docker images
+
+export DEBEZIUM_VERSION=1.4
+
 docker-compose -f docker-compose-mysql.yaml up -d
+
+echo "sleeping for 10s..."; sleep 10s
+
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-mysql.json
 
--- src/main/scala/ConsumerMain.scala
+++ src/main/scala/ConsumerMain.scala
@@ -1,10 +1,9 @@
-//Copyright (c) Microsoft Corporation. All rights reserved.
-//Licensed under the MIT License.
 import akka.actor.ActorSystem
 import akka.kafka.scaladsl.Consumer
 import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
 
 import scala.concurrent.Future
@@ -24,11 +23,20 @@ object ConsumerMain {
     val topic = "dbserver1.courier_order_db.CourierTest"
     val topic2 = "dbserver1.courier_order_db.OrderTest"
 
+    var consumerRecordList = List[ConsumerRecord[Array[Byte], String]]()
+    def processMessage(msg: ConsumerRecord[Array[Byte], String]) = {
+      consumerRecordList = msg :: consumerRecordList
+      println(s"Message Received : ${msg.timestamp} - ${msg.value}")
+      consumerRecordList.foreach {
+        println
+      }
+    }
+
     // listen to our topic with our settings, until the program is exited
     Consumer.plainSource(consumerSettings, Subscriptions.topics(topic, topic2))
       .mapAsync(1) ( msg => {
         // print out our message once it's received
-        println(s"Message Received : ${msg.timestamp} - ${msg.value}")
+        processMessage(msg)
         Future.successful(msg)
       }).runWith(Sink.ignore)
   }
