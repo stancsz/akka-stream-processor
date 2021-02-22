dbcd71813a199ab3aa1b144bd50972394b27d27b
Stan Chen <stanleychen587@gmail.com>
2021-02-22 03:36:55 -0700

    fixed data type conversion error

--- src/main/scala/streamProcessor/RecordProcessor.scala
+++ src/main/scala/streamProcessor/RecordProcessor.scala
@@ -37,22 +37,27 @@ object RecordProcessor {
     print(s"line 40...${event}")
 
     val courier_id = (event \ "courier_id").get.as[String]
-    val courier_score = (event \ "courier_score").get.as[String]
+    val courier_score = (event \ "courier_score").get.as[String].toDouble
     val cour_app_created_timestamp = (event \ "app_created_timestamp").get.as[String]
-    val cour_lat = (event \ "lat").get.as[String]
-    val cour_lon = (event \ "lon").get.as[String]
+    val cour_lat = (event \ "lat").get.as[String].toDouble
+    val cour_lon = (event \ "lon").get.as[String].toDouble
 
 
     def matchRec(record: JsValue): Unit ={
-//      courier_id,courier_score,app_created_timestamp,lat,lon
-      println("line 49 print rec..")
-      val order_id = (record \ "order_id").get.as[String]
-      val order_score = (record \ "order_score").get.as[String]
-      val ord_app_created_timestamp = (record \ "app_created_timestamp").get.as[String]
-      val ord_lat = (record \ "lat").get.as[String]
-      val ord_lon = (record \ "lon").get.as[String]
-      println("line 55 print rec..", order_id,order_score,ord_app_created_timestamp,ord_lat,ord_lon)
-    }
+      try{
+        //      courier_id,courier_score,app_created_timestamp,lat,lon
+        println("line 49 print rec..")
+        val order_id = (record \ "order_id").get.as[String]
+        val order_score = (record \ "order_score").get.as[String].toDouble
+        val ord_app_created_timestamp = (record \ "app_created_timestamp").get.as[String]
+        val ord_lat = (record \ "lat").get.as[String].toDouble
+        val ord_lon = (record \ "lon").get.as[String].toDouble
+        println("line 55 print rec..", order_id,order_score,ord_app_created_timestamp,ord_lat,ord_lon)
+
+      } catch {
+        case e: Exception => print(e)
+      }
+   }
 
     val matched = false
     main.orderRecords.foreach(
